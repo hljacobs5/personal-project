9b774744124ffb9d07df36852aa324fe13b10bff
Haley Jacobs <haleyljacobs@gmail.com>
2018-10-31 20:17:02 -0600

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { NavLink, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>ACTIVIST TRIVIA</h1>
         </header>
+        <main>
+          <h3>Educate yourself about your passion.</h3>   
+          <a href="#">Politics</a> 
+        </main>
       </div>
     );
   }
